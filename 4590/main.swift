1219537400b0fce7cdba1b2180ee5f91d041ccb8
20161104590 <a20161104590@king.local>
2018-09-07 16:13:07 +0800

    Initial Commit

---
+++ 4590/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  4590
+//
+//  Created by 20161104590 on 18/9/7.
+//  Copyright © 2018年 20161104590. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
